6d0c07f08194929d82a040ed17d47d666bc06b50
jeff <jmorton138@gmail.com>
2021-10-19 14:39:09 +0100

    Add unique validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     
-    validates :username, presence: true, length: { in: 6..20 }
-    validates :password, presence: true, length: { in: 6..20 }
+    validates :username, presence: true, length: { in: 5..20 }, uniqueness: true
+    validates :password, presence: true, length: { in: 5..20 }
 
     has_many :posts
     has_many :comments
